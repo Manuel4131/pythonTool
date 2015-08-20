dd4e396d0405f60bcaeef35acffb9747a5895286
Manuel4131 <Manual131419@gmail.com>
2015-08-20 23:44:42 +0800

    Give proper unit to represent the file size

--- FIO/ls/lsFun.py
+++ FIO/ls/lsFun.py
@@ -10,15 +10,15 @@ import os, sys, time;
 def translateUnit(bits):
 	bits=int(bits)
 	d=0
-	units=('B','K','M','G')
+	units=('B','K','M','G','T')
 	while bits >= 1024:			
 		bits/=k
 		d=d+1
-	print "bis are:", bits
-	print "degree is: ", units[d]
+	return repr(bits) + units[0]
+	# return 
+	# print "bis are:", bits
+	# print "degree is: ", units[d]
 	
-
-
 def getDirSize(targetPath):
 	totalSize=0
 	for rootDir, subDir, subFile in os.walk(targetPath):
@@ -27,10 +27,10 @@ def getDirSize(targetPath):
 	return totalSize
 
 def printFile(targetPath):
-	print "{:>10} {:>8} {:>20}".format(targetPath, os.path.getsize(targetPath), time.ctime(os.path.getatime(targetPath)))
+	print "{:>10} {:>8} {:>20}".format(targetPath, translateUnit(os.path.getsize(targetPath)), time.ctime(os.path.getatime(targetPath)))
 
 def printDir(targetPath):
-	print "{:>10} {:>8} {:>20}".format(targetPath, getDirSize(targetPath), time.ctime(os.path.getatime(targetPath)))	
+	print "{:>10} {:>8} {:>20}".format(targetPath, translateUnit(getDirSize(targetPath)), time.ctime(os.path.getatime(targetPath)))	
 	
 def ls(targetPath):	
 	if os.path.isfile(targetPath):
@@ -38,8 +38,8 @@ def ls(targetPath):
 	elif os.path.isdir(targetPath):	
 		printDir(targetPath)
 
-# targetPath=sys.argv[1]
-# ls(targetPath)
-val=sys.argv[1]
-translateUnit(val)
+targetPath=sys.argv[1]
+ls(targetPath)
+# val=sys.argv[1]
+# translateUnit(val)
 

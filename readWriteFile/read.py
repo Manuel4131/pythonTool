f530ff7143cc3dc81d9e3ad308cf26fe30ba92f0
Manuel4131 <Manual131419@gmail.com>
2016-01-01 23:41:33 +0800

    New Year

--- readWriteFile/read.py
+++ readWriteFile/read.py
@@ -3,10 +3,11 @@
 filename=raw_input("Please enter the file name to be accessed:  ")
 mode=raw_input("Please select the mode, w:overwrite, r:readonly, r+:read+write, a:append data to filename:  ")
 try:
- 	f = open(filename,mode)
+    f = open(filename,mode)
 except IOError as e:
- 	print "I/O error {0}, {1}".format(e.errno, e.stderror)
+    print "I/O error {0}, {1}".format(e.errno, e.stderror)
 
-print "f.name",  f.name
-print "f.mode",  f.mode
-print "f.closed", f.closed	
+# Test while
+for line in f:
+	print line,
+f.close()

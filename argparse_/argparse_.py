8927cd5e77ccf10ff88a5436e71aa532e3c7d3a4
Manuel4131 <Manual131419@gmail.com>
2016-06-18 17:03:57 +0800

    Simple test

--- argparse_/argparse_.py
+++ argparse_/argparse_.py
@@ -8,5 +8,5 @@ parser.add_argument('-l', action='append', default=[], dest='ll', help='no way!'
 result=parser.parse_args()
 
 
-for i in range(len(result)):
-    print result[i]
+#for i in range(len(result)):
+#    print result[i]
